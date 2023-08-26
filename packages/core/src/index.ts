16d93dbd8cec0d3e7223f2ca54f437f9a2a5a80f
Sebastian Krüger <sk@honeymachine.io>
2023-08-26 22:02:48 +0200

    fix: jefferson export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/services/smart-watch.service';
 export * from './lib/services/toy.service';
 export * from './lib/toys/fantasy-cup.toy';
+export * from './lib/toys/jefferson.toy';
 export * from './lib/toys/lovense-lush.toy';
 export * from './lib/toys/pulse-union.toy';
 export * from './lib/toys/toy';
